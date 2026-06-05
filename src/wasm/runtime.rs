d75e6e4dfe9a488d09e38d6726b69998bd3501a1
Derek Smith <github@sandy.simplelogin.com>
2026-06-05 14:10:16 -0600

    Add execute method that doesn't work yet, but moving back to the interpreter for a bit

--- src/wasm/runtime.rs
+++ src/wasm/runtime.rs
@@ -27,4 +27,57 @@ impl Runtime {
             call_stack: vec![],
         })
     }
+
+    fn execute(&mut self) -> Result<(), String> {
+        loop {
+            let Some(frame) = self.call_stack.last_mut() else {
+                break;
+            };
+
+            frame.pc += 1;
+
+            let Some(instruction) = frame.instructions.get(frame.pc as usize) else {
+                break;
+            };
+
+            match instruction {
+                Instruction::End => {
+                    self.call_stack.pop();
+
+                    let Frame { sp, arity, ..} = frame;
+                    self.stack_unwind(sp.clone(), arity.clone());
+                },
+                Instruction::LocalGet(index) => {
+                    let Some(value) = frame.locals.get(*index as usize) else {
+                        return Err(panic!("didn't find local index {}", index));
+                    };
+
+                    self.stack.push(*value);
+                },
+                Instruction::I32Add => {
+                    let (Some(right), Some(left)) = (self.stack.pop(), self.stack.pop()) else {
+                        return Err(panic!("not ennough values on that stack for instruction {:?}", instruction));
+                    };
+
+                    self.stack.push(left + right);
+                },
+            }
+        }
+
+        Ok(())
+    }
+
+    fn stack_unwind(&mut self, sp: usize, arity: usize) -> Result<(), String> {
+        if arity > 0 {
+            let Some(value) = self.stack.pop() else {
+                return Err("didn't find return value on the stack".to_string());
+            };
+
+            self.stack.drain(sp..);
+            self.stack.push(value);
+        } else {
+            self.stack.drain(sp..);
+        }
+        Ok(())
+    }
 }
--- src/wasm/value.rs
+++ src/wasm/value.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, Copy)]
 pub enum Value {
     I32(i32),
     I64(i64),
