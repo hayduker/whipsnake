dd7160cba6db1c1f319d2ee393583ee6c18fc928
Derek Smith <github@sandy.simplelogin.com>
2026-07-15 15:55:15 -0600

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Whipsnake compiler development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            clippy
+          ];
+        };
+      });
+}
